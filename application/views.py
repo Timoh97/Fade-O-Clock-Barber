0fdde963a2985a1d451e046dfa977e2e791b4a41
Timoh97 <timohmugendi@gmail.com>
2022-05-24 17:10:34 +0300

    create account activation template

--- application/views.py
+++ application/views.py
@@ -6,6 +6,19 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.core import signing
 from django.template.loader import render_to_string
 
+#registration imports
+from django.http import HttpResponse  
+from django.shortcuts import render, redirect  
+from django.contrib.auth import login, authenticate  
+from .forms import SignupForm  
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_text  
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string  
+from .token import account_activation_token  
+from django.contrib.auth.models import User  
+from django.core.mail import EmailMessage  
+
 
 
 
@@ -31,3 +44,31 @@ def about (request):
     return('about.html',request)
 
 
+#registration
+
+def signup(request):  
+    if request.method == 'POST':  
+        form = SignupForm(request.POST)  
+        if form.is_valid():  
+            # save form in the memory not in database  
+            user = form.save(commit=False)  
+            user.is_active = False  
+            user.save()  
+            # to get the domain of the current site  
+            current_site = get_current_site(request)  
+            mail_subject = 'Activation link has been sent to your email id'  
+            message = render_to_string('acc_active_email.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+                'token':account_activation_token.make_token(user),  
+            })  
+            to_email = form.cleaned_data.get('email')  
+            email = EmailMessage(  
+                        mail_subject, message, to=[to_email]  
+            )  
+            email.send()  
+            return HttpResponse('Please confirm your email address to complete the registration')  
+    else:  
+        form = SignupForm()  
+    return render(request, 'signup.html', {'form': form}) 
