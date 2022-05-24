ccd28d94e8670c3f081767189dbba7326c0e0ccf
Timoh97 <timohmugendi@gmail.com>
2022-05-25 00:27:43 +0300

    create screenshots folder to preview my project

--- application/urls.py
+++ application/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from . import views
 from . views import activate
 urlpatterns = [
-path('index/', views.index, name="index"),
+path('', views.index, name="index"),
 path('about/', views.about, name="about"),
 path('contact/', views.contact, name="contact"),
 path('pricing/', views.pricing, name="pricing"),
