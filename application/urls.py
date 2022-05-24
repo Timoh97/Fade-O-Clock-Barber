06044d25f26e16e6c44fc848bbb80c0b93c89845
Timoh97 <timohmugendi@gmail.com>
2022-05-24 17:06:24 +0300

    create token.py file

--- application/urls.py
+++ application/urls.py
@@ -1,9 +1,36 @@
 from django.urls import path,include
+from django.views.generic.base import TemplateView
 from . import views
 urlpatterns = [
-path('', views.index, name="index"),
+path('index/', views.index, name="index"),
 path('about/', views.about, name="about"),
 path('contact/', views.contact, name="contact"),
 path('pricing/', views.pricing, name="pricing"),
 path('services/', views.services, name="services"),
+
+#registration
+path('activate/complete/',
+        TemplateView.as_view(
+            template_name='registration/activation_complete.html'
+        ),
+        name='registration_activation_complete'),
+    # The activation key can make use of any character from the
+    # URL-safe base64 alphabet, plus the colon as a separator.
+path('activate/(?P<activation_key>[-:\w]+)/',
+        views.ActivationView.as_view(),
+        name='registration_activate'),
+path('register/',
+        views.RegistrationView.as_view(),
+        name='registration_register'),
+path(r'register/complete/',
+        TemplateView.as_view(
+            template_name='registration/registration_complete.html'
+        ),
+        name='registration_complete'),
+path('register/closed/',
+        TemplateView.as_view(
+            template_name='registration/registration_closed.html'
+        ),
+        name='registration_disallowed'),
+path(r'', include('registration.auth_urls'))
 ]
--- application/views.py
+++ application/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.sites.shortcuts import get_current_site
+from django.core import signing
+from django.template.loader import render_to_string
+
+
+
+
+
 # Create your views here.
 
 def index(request):
@@ -19,3 +29,5 @@ def services (request):
 
 def about (request):
     return('about.html',request)
+
+
