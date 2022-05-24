df7fb22104e0de29c16d5428ef61626ba69ff26a
Timoh97 <timohmugendi@gmail.com>
2022-05-24 17:13:08 +0300

    create activation view

--- application/views.py
+++ application/views.py
@@ -72,3 +72,18 @@ def signup(request):
     else:  
         form = SignupForm()  
     return render(request, 'signup.html', {'form': form}) 
+
+#create activation view
+def activate(request, uidb64, token):  
+    User = get_user_model()  
+    try:  
+        uid = force_text(urlsafe_base64_decode(uidb64))  
+        user = User.objects.get(pk=uid)  
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
+        user = None  
+    if user is not None and account_activation_token.check_token(user, token):  
+        user.is_active = True  
+        user.save()  
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')  
+    else:  
+        return HttpResponse('Activation link is invalid!') 
